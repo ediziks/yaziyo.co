3b71e4dcdaf0d1183584b43fbf8c2a4da7b9ae03
eemektas <eemektas@gmail.com>
2021-02-10 05:28:12 +0300

    readtime tr fix

--- bloggy/templatetags/readtime.py
+++ bloggy/templatetags/readtime.py
@@ -1,11 +1,14 @@
 from django import template
 import readtime
+import math
 
 register = template.Library()
 
 
 def read(html):
-  return readtime.of_html(html)
+  with_text = readtime.of_html(html)
+  result = math.ceil(with_text.seconds / 60)
+  return result
 
 
 register.filter('readtime', read)
